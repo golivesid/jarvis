2c762439173d8e2dac076253779246356b0eb659
golivesid <155578467+golivesid@users.noreply.github.com>
2024-03-03 10:08:39 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function getDetails(id) {
     try {
         const response = await axios.get(
-            `https://6229f56a-ea18-4a07-b514-e8d8d32b7b36-00-15pr5d1nprwqy.sisko.replit.dev/terabox?url=${id}`
+            `https://987ff9a4-78ea-4f74-9e9d-c2ad86fbe9c8-00-wajfvnw72zdl.sisko.replit.dev/terabox?url=${id}`
         );
         return response.data;
     } catch (error) {
