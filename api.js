b848a3d46c0f09e188196d039c6578ac8c79debb
golivesid <155578467+golivesid@users.noreply.github.com>
2024-03-12 01:10:18 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function getDetails(id) {
     try {
         const response = await axios.get(
-            `https://987ff9a4-78ea-4f74-9e9d-c2ad86fbe9c8-00-wajfvnw72zdl.sisko.replit.dev/terabox?url=${id}`
+            'https://97230462-11c2-4345-861d-ad5a111cb134-00-3l5og8lb3sk19.sisko.replit.dev/terabox?url=${id}`
         );
         return response.data;
     } catch (error) {
