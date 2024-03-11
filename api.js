076fdbd524985efd428e4cf9b650d01b31ede46c
golivesid <155578467+golivesid@users.noreply.github.com>
2024-03-12 01:38:35 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function getDetails(id) {
     try {
         const response = await axios.get(
-            'https://97230462-11c2-4345-861d-ad5a111cb134-00-3l5og8lb3sk19.sisko.replit.dev/terabox?url=${id}`
+            'https://terabox-downloader.p.rapidapi.com/?url=${id}`
         );
         return response.data;
     } catch (error) {
